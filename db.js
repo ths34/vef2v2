1ce517618c5703f8478e878073fd8f689f124029
ths34 <42776696+ths34@users.noreply.github.com>
2019-02-06 13:07:31 +0000

    Update db.js

--- db.js
+++ db.js
@@ -4,4 +4,26 @@ const connectionString = process.env.DATABASE_URL; // sótt úr env gegnum doten
 
 /* todo útfæra */
 
-module.exports =
+const client = new Client({
+  connectionString,
+});
+
+client.connect();
+async function insert(name) {
+    try {
+      const query = 'INSERT INTO applications (name, email, phonenumber, text, job, processed, created, updated) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
+      const res = await client.query(query, values);
+      console.log(res.rows);
+    } catch (e) {
+      console.error('Error selecting', e);
+    } finally {
+      await client.end(); // alltaf keyrt
+    }
+}
+ // insert().catch(e => console.error(e));
+  console.log("tenging næst við ganggrunn!");
+  client.end();
+
+module.exports = {
+  insert,
+};
