52cbefd8573951146aed36df113867ed505d7624
ths34 <42776696+ths34@users.noreply.github.com>
2019-02-06 13:08:30 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,12 @@
 CREATE TABLE applications (
-  -- TODO schema fyrir töflu
+  id serial primary key,
+  name varchar(255) not null unique,
+  email varchar(255) not null,
+  phonenumber int,
+  text text, 
+  job varchar(100),
+  processed boolean default false,
+  created timestamp with time zone not null default current_timestamp,
+  updated timestamp with time zone not null default current_timestamp
 );
+
